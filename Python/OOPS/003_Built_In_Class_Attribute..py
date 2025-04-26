3d7d1c134b508886f3d182aa79c9cd8ebeb8ff1c
Anoop-Kumar-Yadav <165376785+Anoop-Kumar-Yadav@users.noreply.github.com>
2025-04-26 23:50:16 +0530

    demonstrated use of 4 built-in class attributres..

---
+++ Python/OOPS/003_Built_In_Class_Attribute..py
@@ -0,0 +1,92 @@
+'''
+__dict__   : Dictionary containing class's namespace
+__doc__    : Class Documentation string
+__class__   : Class Name
+__module__ : Module namein which class is defined
+__bases__  : List of base classes
+
+'''
+
+
+class Car:
+    """
+    A class to represent a Car.
+
+    Attributes:
+    -----------
+    company : str
+        The manufacturer of the car (e.g., Toyota, Ford).
+    model : str
+        The model name of the car (e.g., Corolla, Mustang).
+    color : str
+        The color of the car.
+    speed : int
+        The max speed of car
+
+    Methods:
+    --------
+    new_launched():
+        Prints the car's color, make, and model.
+    
+    Start():
+        Simulates Starting the car by printing a message.
+
+    Drive():
+        Simulates driving the car by printing a message.
+
+    Stop():
+        Simulates Stopping the car by printing a message.
+
+    Accelerate():
+        Simulates Accelerating the car by printing a message.
+    """
+
+    def __init__(self,color,company,model,speed):
+        self.color = color
+        self.company = company
+        self.model = model
+        self.speed = speed
+        self.new_launched()
+
+    def new_launched(self):
+        print("-------------------------------------")
+        print("New Car Launched")
+        print(self.__dict__)
+        print("-------------------------------------")
+
+    def Start(self):
+        print(f"{self.model} Starting...")
+
+    def Drive(self):
+        self.Start()
+        self.Accelerate()
+        print(f"{self.model} Driving...")
+        
+        
+    def Stop(self):
+        print(f"{self.model}Stopping...")
+        print("Speed : ",end=" ")
+        for i in range(self.speed,-1,-1):
+            print(i,"<-",end="")
+        print()
+
+    def Accelerate(self):
+        print(f"{self.model} Accelerating...")
+        print("Speed : ",end=" ")
+        for i in range(0,self.speed+1):
+            print("->",i,end="")
+        print()
+
+
+
+c1 = Car(color="Green",model="WAGON-R",company="TATA",speed=9)
+
+print(c1.__doc__)
+print(c1.__dict__) #{'color': 'Green', 'company': 'TATA', 'model': 'WAGON-R', 'speed': 9}
+print(c1.__module__) #__main__
+print(c1.__class__) #<class '__main__.Car'>
+
+
+
+
+
